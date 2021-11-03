a0566fca3d946df5a10256faf70a4b2f33afee13
Piotr Mankowski <pmanko@uw.edu>
2021-11-02 22:02:35 -0700

    Fixes

--- src/lib/shrMediator.ts
+++ src/lib/shrMediator.ts
@@ -6,23 +6,23 @@ import shrApp from './shr'
 
 const medUtils = require('openhim-mediator-utils')
 
+const env = process.env.NODE_ENV || 'development'
+const medConfig = require(`${__dirname}/../../config/mediator_${env}`)
+const appConfig = require(`${__dirname}/../../config/config_${env}`);
 
 export class ShrMediator {
-  private config: any
-  private appConfigFile: any
+  private config: JSON
   private callback: Function
 
   constructor(callback: Function) {
-    const env = process.env.NODE_ENV || 'development';
-    this.appConfigFile = require(`${__dirname}/../config/config_${env}`);
-    this.config = require(`${__dirname}/../config/mediator_${env}`);
     this.callback = callback
+    this.config = medConfig
   }
 
   public start() {
     logger.info('Running SHR as a mediator with' + `${__dirname}/${this.config}`)
     try {
-      medUtils.registerMediator(config.get('mediator:api'), this.config, this.registrationCallback)
+      medUtils.registerMediator(config.get('mediator:api'), this.config, ShrMediator.registrationCallback(this.callback))
     } catch (e: any) {
       logger.error(`Could not start SHR as a Mediator!\n${JSON.stringify(e)}`)
       process.exit(1)
@@ -30,66 +30,72 @@ export class ShrMediator {
 
   }
 
-  private registrationCallback(err: Error | null) {
-    if (err) {
-      logger.error('Failed to register mediator at ' + config.get('mediator:api:apiURL') + '\nCheck your config!\n');
-      logger.error(err.stack!)
-      process.exit(1);
-    } else {
-      config.set('mediator:api:urn', this.config.urn);
+  private static registrationCallback(callback: Function) {
+    return (err: Error | null) => {
+      if (err) {
+        logger.error('Failed to register mediator at ' + config.get('mediator:api:apiURL') + '\nCheck your config!\n');
+        logger.error(err.stack!)
+        process.exit(1);
+      } else {
+        config.set('mediator:api:urn', medConfig.urn);
 
-      medUtils.fetchConfig(config.get('mediator:api'), this.setupCallback)
+        medUtils.fetchConfig(config.get('mediator:api'), ShrMediator.setupCallback(callback))
+      }
     }
   }
 
-  private setupCallback(err: Error | null, initialConfig: JSON) {
-    if (err) {
-      logger.info('Failed to fetch initial config');
-      process.exit(1);
+  private static setupCallback(callback: Function) {
+    return (err: Error | null, initialConfig: JSON) => {
+      if (err) {
+        logger.info('Failed to fetch initial config');
+        process.exit(1);
+      }
+
+      // Merges configs?
+      const updatedConfig: JSON = Object.assign(appConfig, initialConfig);
+      logger.info('Received initial config:', initialConfig);
+
+      this.reloadConfig(updatedConfig, ShrMediator.startupCallback(callback));
     }
-
-    // Merges configs?
-    const updatedConfig: JSON = Object.assign(this.appConfigFile, initialConfig);
-
-    this.reloadConfig(updatedConfig, this.startupCallback);
   }
 
-  private startupCallback(initialConfig: JSON) {
-    config.set('mediator:api:urn', this.config.urn);
-    logger.info('Received initial config:', initialConfig);
-    logger.info('Successfully registered mediator!');
+  private static startupCallback(callback: Function) {
+    return () => {
+      config.set('mediator:api:urn', medConfig.urn);
+      logger.info('Successfully registered mediator!');
 
-    const app = shrApp()
-    const port = config.get('app:port')
+      const app = shrApp()
+      const port = config.get('app:port')
 
-    // Start up server on 3000 (default)
-    const server = app.listen(port, () => {
+      // Start up server on 3000 (default)
+      const server = app.listen(port, () => {
 
-      // Activate heartbeat for OpenHIM mediator
-      const configEmitter = medUtils.activateHeartbeat(config.get('mediator:api'));
+        // Activate heartbeat for OpenHIM mediator
+        const configEmitter = medUtils.activateHeartbeat(config.get('mediator:api'));
 
-      // Updates config based on what's sent from the server
-      configEmitter.on('config', this.updateCallback);
+        // Updates config based on what's sent from the server
+        configEmitter.on('config', ShrMediator.updateCallback);
 
-      // Runs initial callback
-      this.callback(server);
-    });
+        // Runs initial callback
+        callback(server);
+      });
+    }
   }
 
-  private updateCallback(newConfig: JSON) {
+  private static updateCallback(newConfig: JSON) {
 
     logger.info('Received updated config:', newConfig);
 
-    const updatedConfig = Object.assign(this.appConfigFile, newConfig);
+    const updatedConfig = Object.assign(appConfig, newConfig);
 
-    this.reloadConfig(updatedConfig, () => {
-      config.set('mediator:api:urn', this.config.urn);
+    ShrMediator.reloadConfig(updatedConfig, () => {
+      config.set('mediator:api:urn', medConfig.urn);
     });
 
   }
 
-  private reloadConfig(data: JSON, callback: Function) {
-    const tmpFile = `${__dirname}/../config/tmpConfig.json`;
+  private static reloadConfig(data: JSON, callback: Function) {
+    const tmpFile = `${__dirname}/../../config/tmpConfig.json`;
 
     fs.writeFile(tmpFile, JSON.stringify(data), (err: Error | null) => {
       if (err) {
